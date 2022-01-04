175be6d36374115e303ee364d9ed440522f73449
Xamidullo Xudoyberdiyev <xamidullox@swiftrelay.io>
2022-01-05 00:22:20 +0500

    Go (Golang) Tutorial #4 - Printing & Formatting Strings

--- main.go
+++ main.go
@@ -3,5 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hwllo Golang!")
+	age := 21
+	name := "Xamidullo"
+	
+	//Print
+	fmt.Print("Hello ")
+	fmt.Print("Golang! \n")
+	fmt.Print("new Line! \n")
+
+	fmt.Println("Hello ninjas!")
+	fmt.Println("Goodbye ninjas!")
+	fmt.Println("My age is ", age, "and My name is ",name)
+
+	//Print (formatted string) %_ = format specifer
+	fmt.Printf("my age is %v and my name is %v \n",age, name)
+	fmt.Printf("my age is %v and my name is %q \n",age, name)
+	fmt.Printf("age is of type %T \n",age)
+	fmt.Printf("you scored %f points! \n", 222.25)
+
+	// fmt.Sprint(save formatted string)
+	var str = fmt.Sprintf("my age is %v and my name is %v \n",age, name)
+
+	fmt.Println("the saved string is :",str)
 }
