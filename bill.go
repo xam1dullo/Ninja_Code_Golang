64e7eacd7602bae6f8b7468e2c77edb847ad54d5
Xamidullo Xudoyberdiyev <xamidullox@swiftrelay.io>
2022-01-07 13:06:33 +0500

    initial commit

--- bill.go
+++ bill.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type bill struct {
 	name  string
 	items map[string]float64
@@ -12,6 +14,33 @@ func newBill(name string) bill {
 		name:  name,
 		items: map[string]float64{},
 		tip:   0,
-	}
+	 } 
 	return b
 }
+
+// format the bill
+func (b *bill) format() string {
+	fs := "Bill breakdown: \n"
+	var total float64 = 0;
+	//list all items
+	for k, v := range b.items {
+		fs += fmt.Sprintf("%-25v ...$%v \n", k+":", v)
+		total += v
+	}
+	// add tip
+	fs += fmt.Sprintf("%-25v ...$%v\n", "tip:", b.tip)	
+	
+	//total
+	fs += fmt.Sprintf("%-25v ...$%0.2f", "totoal:", total + b.tip)
+	
+	return fs
+	
+	} 
+	// update tip
+	func (b *bill)upadateTip(tip float64){
+		b.tip = tip
+	}
+	//add an item to the bill
+	func (b bill )addItem(name string, price float64){
+		b.items[name] = price
+	}
--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	myBill := newBill("Bill")
-	fmt.Println(myBill)
+	mybill := newBill("mario's bill")
+
+	mybill.addItem("pie", 5.88)
+	mybill.addItem("cake", 1.88)
+	mybill.addItem("coffe", 2.77)
+	mybill.addItem("toffee", 4.95)
+	mybill.addItem("onion soup", 4.53)
+	mybill.upadateTip(10)
+
+	fmt.Println(mybill.format())
 }
