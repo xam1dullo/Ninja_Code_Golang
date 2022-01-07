2fe74bf36877dd0090f0abaa977e403ed17ee9c6
Xamidullo Xudoyberdiyev <xamidullox@swiftrelay.io>
2022-01-07 11:51:10 +0500

    initial commit

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## lesson# 14
 
 - Go (Golang) Tutorial #14 - Pointers
+
+* go run main.go bill.go
--- main.go
+++ main.go
@@ -2,30 +2,7 @@ package main
 
 import "fmt"
 
-func updateName(x *string) {
-	*x = "azizbek"
-}
-
 func main() {
-	// gruop A = type  -> string, int, float, bool, arrays, structs
-	name := "xamidullo"
-	
-	// updateName(name)
-
-	fmt.Println("memory address of name: ", &name)
-	m := &name
-	fmt.Println("memory address : ", m)
-	fmt.Println("value of namae: ", *m)
-	updateName(m)
-	fmt.Println("value of name: ", *m)
-
-	// gruop B = types => slices, maps, functions
-/* 
-|-----name----|------m------|
-| 0xc0000a0a0 | 0xc0000a0a0 |
-| azizbek     | xamidullo   |
-|-------------|-------------|
-
-*/
-	
+	myBill := newBill("Bill")
+	fmt.Println(myBill)
 }
