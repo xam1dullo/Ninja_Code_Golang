d1df5b607b46e52f42e595fbe2ffda1a6098468f
Xamidullo Xudoyberdiyev <xamidullox@swiftrelay.io>
2022-01-07 13:27:04 +0500

    initial commit

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Ninja_Code_Golang
 
-## lesson# 17
+## lesson# 18
 
-- Go (Golang) Tutorial #17 - Receiver Functions with Pointers
+- Go (Golang) Tutorial #18 - User Input
 
 * go run main.go bill.go
--- main.go
+++ main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
+func getInput(prompt string, r *bufio.Reader)(string, error){
+	fmt.Print(prompt)
+	input , err := r.ReadString('\n')
+	return strings.TrimSpace(input), err
+}
+
+func createBill() bill {
+	reader :=bufio.NewReader(os.Stdin)
+	
+	name, _ := getInput("Create a new bill name: ", reader)
+	b := newBill(name)	
+	fmt.Println("Create the bill -", b.name)
+	return b
+}
+func promptOptions (b bill){
+	reader := bufio.NewReader(os.Stdin)
+	opt, _ := getInput("Choose option (a -add item, s - save bill, s -add tip): ", reader)
+	fmt.Println("Option: ", opt)
+}
 
 func main() {
-	mybill := newBill("mario's bill")
-
-	mybill.addItem("pie", 5.88)
-	mybill.addItem("cake", 1.88)
-	mybill.addItem("coffe", 2.77)
-	mybill.addItem("toffee", 4.95)
-	mybill.addItem("onion soup", 4.53)
-	mybill.upadateTip(10)
-
-	fmt.Println(mybill.format())
+	mybill := createBill()
+	promptOptions(mybill)
+	fmt.Println(mybill)
 }
